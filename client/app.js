0a0062008cc0cfc8c43c6942d7a759201b686956
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-11 13:06:59 +0100

    update proc

--- client/app.js
+++ client/app.js
@@ -1,13 +1,13 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+// const http = require('http');
+// const port=process.env.PORT || 3000
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+// server.listen(port,() => {
+//   console.log(`Server running at port `+port);
+// });
 
 const BattleShipForm = require('./src/views/battleship_form_view.js');
 const BattleShipNewGame = require('./src/models/new_game.js');
